b7109dffb1f6f62477e7ccb9f36a701d29be41f7
Selin Dursun <selindursunus@gmail.com>
2024-04-09 16:20:05 -0400

    Update App.jsx

--- x-design/src/App.jsx
+++ x-design/src/App.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber"
 import "./App.css"
-import { useCallback, useRef } from "react"
+import { useRef } from "react"
 import { OrbitControls, MeshWobbleMaterial, useHelper } from '@react-three/drei'
 import { DirectionalLightHelper } from "three"
 import { useControls } from "leva"
@@ -8,35 +8,33 @@ import React, { useState } from 'react';
 
 
 
-
-// const Cube = ({ position, size, color }) => {
-//   const ref = useRef()
-
+const Cube = ({ position, size, color }) => {
+  const ref = useRef()
 
 
-//   useFrame((state, delta) => {
-//     ref.current.rotation.x += delta  //difference in time between the current frame and the last frame
-//     // console.log(state)
-//     ref.current.rotation.y += delta * 2.0
-//     ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
-//     console.log(state.clock.elapsedTime)
-//   })
 
-//   return (
-//     <mesh position={position} ref={ref}>
-//     <boxGeometry args ={size}  />
-//     <meshStandardMaterial color={color}/>
-//   </mesh>
-//   )
-// }
-
-// const Sphere = ({ position, size, color }) => {
-
-//   const ref = useRef()
-
-<<<<<<< HEAD
   useFrame((state, delta) => {
-    ref.current.rotation.x  += delta  //difference in time between the current frame and the last frame
+    ref.current.rotation.x += delta  //difference in time between the current frame and the last frame
+    // console.log(state)
+    ref.current.rotation.y += delta * 2.0
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
+    console.log(state.clock.elapsedTime)
+  })
+
+  return (
+    <mesh position={position} ref={ref}>
+    <boxGeometry args ={size}  />
+    <meshStandardMaterial color={color}/>
+  </mesh>
+  )
+}
+
+const Sphere = ({ position, size, color }) => {
+
+  const ref = useRef()
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta  //difference in time between the current frame and the last frame
     // console.log(state)
     // ref.current.rotation.y += delta * 2.0
     // console.log(state.clock.elapsedTime)
@@ -45,104 +43,66 @@ import React, { useState } from 'react';
   return (
     <mesh position = {position} ref={ref}> 
       <sphereGeometry args = {size} />
-      <meshStandardMaterial color = {color} wireframe = {true}/>
-=======
-//   useFrame((state, delta) => {
-//     ref.current.rotation.x += delta  //difference in time between the current frame and the last frame
-//     // console.log(state)
-//     // ref.current.rotation.y += delta * 2.0
-//     // console.log(state.clock.elapsedTime)
-//   })
+      <meshStandardMaterial color = {color} wireframe/>
 
-//   return (
-//     <mesh position = {position} ref={ref}> 
-//       <sphereGeometry args = {size} />
-//       <meshStandardMaterial color = {color} wireframe/>
->>>>>>> 7c872c8ba2a38e236967eb4e696e9a53e1216784
+    </mesh>
+  )
+}
 
-//     </mesh>
-//   )
-// }
-
-<<<<<<< HEAD
 const Torus = ({ position, size, color }) => {
   return (
-    <mesh position = {position} rotation = {[Math.PI / 2, 0, 0]} >
+    <mesh position = {position}>
       <torusGeometry args = {size} />
       <meshStandardMaterial color = {color} wireframe/>
     </mesh>
   )
 }
-=======
-// const Torus = ({ position, size, color }) => {
-//   return (
-//     <mesh position = {position}>
-//       <torusGeometry args = {size} />
-//       <meshStandardMaterial color = {color} wireframe/>
-//     </mesh>
-//   )
-// }
->>>>>>> 7c872c8ba2a38e236967eb4e696e9a53e1216784
 
-// const TorusKnot = ({ position, size}) => {
+const TorusKnot = ({ position, size}) => {
 
-//   const ref = useRef()
+  const ref = useRef()
 
-//   const {color, radius} = useControls({
-<<<<<<< HEAD
-//     color: "pink",
-=======
-//     color: "lightblue",
->>>>>>> 7c872c8ba2a38e236967eb4e696e9a53e1216784
-//     radius: {
-//       min: 1,
-//       max: 10,
-//       step: 0.5,
-//     }
-//   })
-//   // useFrame((state, delta) => {
-//   //   ref.current.rotation.x += delta  //difference in time between the current frame and the last frame
-//   //   console.log(state)
-//   //   ref.current.rotation.y += delta * 2.0
-//   //   ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
-//   //   console.log(state.clock.elapsedTime)
-//   // })
+  const {color, radius} = useControls({
+    color: "lightblue",
+    radius: {
+      min: 1,
+      max: 10,
+      step: 0.5,
+    }
+  })
+  // useFrame((state, delta) => {
+  //   ref.current.rotation.x += delta  //difference in time between the current frame and the last frame
+  //   console.log(state)
+  //   ref.current.rotation.y += delta * 2.0
+  //   ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
+  //   console.log(state.clock.elapsedTime)
+  // })
 
-//   return (
-//     <mesh position = {position} ref ={ref}>
-//       <torusKnotGeometry args = {size} />
-//       {/* <meshStandardMaterial color = {color} /> */}
-//       <MeshWobbleMaterial factor={2} speed={0.8} color={color}/>
-//     </mesh>
-//   )
-// }
+  return (
+    <mesh position = {position} ref ={ref}>
+      <torusKnotGeometry args = {size} />
+      {/* <meshStandardMaterial color = {color} /> */}
+      <MeshWobbleMaterial factor={2} speed={0.8} color={color}/>
+    </mesh>
+  )
+}
 
 
-const TorusKnotAvatar = ({ wobbleIntensity, thickness, color, posx, posy, posz }) => {
+const TorusKnotAvatar = ({ wobbleIntensity, thickness, color }) => {
   const ref = useRef();
-  console.log(posx);
-  console.log(posy);
-  // let pos = [posx, posy, 0]
 
    // Mapping the input values to the required ranges
    const mappedWobbleIntensity = 1 + (wobbleIntensity / 100) * 9; // Maps 0-100 to 1-10
    const mappedThickness = 0.1 + (thickness / 100) * 0.6; // Maps 0-100 to 0.1-0.7
 
    useFrame(() => {
-    // posx+=.01*Math.random();
-    // posy+=.01*Math.random();
     ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
-    ref.current.position.set(posx, posy, posz);
   });
 
   return (
     <mesh ref={ref}>
-<<<<<<< HEAD
-      <torusKnotGeometry args={[1.5, mappedThickness, 2000, 20]} />
-=======
-      <torusKnotGeometry args={[.25, mappedThickness, 2000, 20]} />
->>>>>>> 7c872c8ba2a38e236967eb4e696e9a53e1216784
+      <torusKnotGeometry args={[1.7, mappedThickness, 2000, 20]} />
       <MeshWobbleMaterial factor={mappedWobbleIntensity} color={color} speed={2} />
     </mesh>
   );
@@ -150,50 +110,7 @@ const TorusKnotAvatar = ({ wobbleIntensity, thickness, color, posx, posy, posz }
 
 }
 
-const SphereAvatar = ({ wobbleIntensity, thickness, color, posx, posy, posz }) => {
-  const ref = useRef();
-  console.log(posx);
-  console.log(posy);
-  // let pos = [posx, posy, 0]
-
-   // Mapping the input values to the required ranges
-   const mappedWobbleIntensity = 1 + (wobbleIntensity / 100) * 9; // Maps 0-100 to 1-10
-   const mappedThickness = 0.1 + (thickness / 100) * 0.6; // Maps 0-100 to 0.1-0.7
-
-  //  useFrame(() => {
-  //   // posx+=.01*Math.random();
-  //   // posy+=.01*Math.random();
-  //   // ref.current.rotation.x += 0.01;
-  //   // ref.current.rotation.y += 0.01;
-  //   ref.current.position.set(posx, posy, posz);
-  // });
-
-  return (
-    <mesh ref={ref}>
-      <sphereGeometry args={[mappedThickness, 32, 32]} />
-      {/* <MeshWobbleMaterial factor={mappedWobbleIntensity} color={color} speed={2} /> */}
-    </mesh>
-  );
-
-
-}
-
-const Scene = ({avatars, section}) => {
-
-  console.log(avatars);
-
-  
-  if(avatars.length > 1) {
-    for(var i=0; i<avatars.length; i++) {
-      console.log(section);
-      console.log(avatars[i].section);
-      console.log((avatars[i].section) == 1);
-      console.log((avatars[i].section) == 2);
-      console.log((avatars[i].section) == 3);
-      console.log((avatars[i].section) == section);
-    };
-    
-  };
+const Scene = ({wobbleIntensity, thickness, color}) => {
 
   const directionalLightRef = useRef()
 
@@ -207,43 +124,34 @@ const Scene = ({avatars, section}) => {
     }
   })
 
-  // useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "white")
-
-
-
+  useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "white")
 
   return (
     <>
       <directionalLight position={[0, 0, 2]} intensity ={lightIntensity} ref={directionalLightRef} color = {lightColor}/>
       <ambientLight intensity={0.4} />
 
-      {avatars.map((avatar, index) => (
-                <mesh
-                key={index}
-                >
-                  {avatar.section == 1 && section == 1 ? <TorusKnotAvatar wobbleIntensity={avatar.wobbleIntensity} thickness={avatar.thickness} color={avatar.color} posx={avatar.posx}  posy={avatar.posy} posz={avatar.posz} /> : null }
-                  {avatar.section == 2 && section == 2 ? <TorusKnotAvatar wobbleIntensity={avatar.wobbleIntensity} thickness={avatar.thickness} color={avatar.color} posx={avatar.posx}  posy={avatar.posy} posz={avatar.posz} /> : null }
-                  {avatar.section == 3 && section == 3 ? <TorusKnotAvatar wobbleIntensity={avatar.wobbleIntensity} thickness={avatar.thickness} color={avatar.color} posx={avatar.posx}  posy={avatar.posy} posz={avatar.posz} /> : null }
+ {/* i have been trying some basic shapes as well hihi  */}
+ 
+       {/* <group position = {[0, -1, 0]}>
+       <Cube position={[1,0,0]} color = {"green"} size ={[1, 1, 1]} />
+       <Cube position = {[-1, 0, 0]} color = {"purple"} size = {[1, 1, 1]} />
+       <Cube position = {[-1, 2, 0]} color = {"hotpink"} size = {[1, 1, 1]} />
+       <Cube position = {[1, 2, 0]} color = {"yellow"} size = {[1, 1, 1]} />
+       </group> */}
 
-                  {/* <TorusKnotAvatar wobbleIntensity={avatar.wobbleIntensity} thickness={avatar.thickness} color={avatar.color} posx={avatar.posx}  posy={avatar.posy} posz={avatar.posz}/> */}
-                </mesh>
-            ))}
+      {/* <Cube position = {[0, 0, 0]} size = {[1, 1, 1]} color = {"orange"} /> */}
 
-<<<<<<< HEAD
-      <Sphere position={[0,1.5,0.8]} size = {[1, 30, 30]} color = {"lightblue"} />
-      <Torus position={[0,3,0]} size = {[0.8, 0.1, 3000, 30]} color = {"blue"}/>
+      {/* <Sphere position={[0,0,0]} size = {[1, 30, 30]} color = {"orange"}/> */}
+      {/* <Torus position={[2,0,0]} size = {[0.8, 0.1, 30, 30]} color = {"blue"}/> */}
       {/* <TorusKnot position={[-2,0,0]} size = {[1, 0.7, 1000, 50]} color = {"hotpink"}/>  */}
 
       <TorusKnotAvatar wobbleIntensity={wobbleIntensity} thickness={thickness} color={color} />
 
       <OrbitControls enableZoom = {false} />
-=======
-      <OrbitControls enableZoom = {true} autoRotate={true} autoRotateSpeed={1}/>
->>>>>>> 7c872c8ba2a38e236967eb4e696e9a53e1216784
       </>
   )
 }
-
 
 
 const App = ()  => {
@@ -252,20 +160,13 @@ const App = ()  => {
     const [wobbleIntensity, setWobbleIntensity] = useState(50);
     const [thickness, setThickness] = useState(50);
     const [color, setColor] = useState("#ff00ff");
-    const [section, setSection] = useState(1);
-    const [avatars, setAvatars] = useState([]);
 
+   
     const sliderStyle = {
     width: '200px', 
     margin: '10px 0', //vertical spacing
   };
 
-
-  // const generateAvatar = useCallback((e, index) => {
-  //   console.log(avatars);
-  //   setAvatars([...avatars, {wobble: wobbleIntensity, thick: thickness, col: color}])
-  //   console.log(wobbleIntensity);
-  // }, [avatars])
 
   // return (
   //   <Canvas>
@@ -274,18 +175,7 @@ const App = ()  => {
   // )
 
   return (
-  <div style={{ width: "100vw", height: "100vh" }}>
     <div>
-    <label>Section {section}</label>
-      <input
-        type="range"
-        min="1"
-        max="3"
-        value={section}
-        onChange={(e) => setSection(e.target.value)}
-        style={sliderStyle}
-      />
-      
       <label>Wobble Intensity: {wobbleIntensity}</label>
       <input
         type="range"
@@ -309,32 +199,14 @@ const App = ()  => {
         value={color}
         onChange={(e) => setColor(e.target.value)}
       />
-
-      <button onClick={() => {
-        setAvatars([
-          ...avatars,
-          {section: section, wobbleIntensity: wobbleIntensity, thickness: thickness, color: color, posx: (Math.random()-.5)*10, posy:(Math.random()-.5)*10, posz: (Math.random()-.5)*10}
-        ]);
-        // console.log(wobbleIntensity);
-      }}> generate</button>
-
-      </div>
-      
-      <div style ={{display: "flex"}}>
-        <Canvas style={{ background: "black", width: "33.3vw", height: "100vh"}} camera={{ position: [-5, 12, 13], fov: 50}}>
-          <Scene avatars={avatars} section={1}/>
-        </Canvas>
-        <Canvas style={{ background: "black", width: "33.3vw", height: "100vh" }} camera={{ position: [-5, 12, 13], fov: 50}}>
-        <Scene avatars={avatars} section={2}/>
-        </Canvas>
-        <Canvas style={{ background: "black", width: "33.3vw", height: "100vh" }} camera={{ position: [-5, 12, 13], fov: 50}}>
-        <Scene avatars={avatars} section={3}/>
-        </Canvas>
-      </div>
-
+      <Canvas>
+        <Scene wobbleIntensity={wobbleIntensity} thickness={thickness} color={color} />
+      </Canvas>
     </div>
   );
 
 }
 
-export default App;
+
+
+export default App
